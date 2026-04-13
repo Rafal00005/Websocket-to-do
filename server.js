5337b5621b19ae860e9b4a473880bfef781045b2
Rafal00005 <rafalniestrzeba@gmail.com>
2026-04-13 13:03:45 +0200

    add server socket.io logic

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+
 const server = app.listen(8000, () => {
 	console.log('Server is running...');
 });
 
+// integrate socket.io with our server
 const io = require('socket.io')(server, { cors: { origin: '*' } });
 
+// array of tasks stored on the server
 let tasks = [];
 
+// listen for new client connections
 io.on('connection', (socket) => {
 	console.log('New client connected:', socket.id);
+
+	// send current tasks to the newly connected client
+	socket.emit('updateData', tasks);
+
+	// listen for adding a task
+	socket.on('addTask', (task) => {
+		tasks.push(task); // add task to server array
+		socket.broadcast.emit('addTask', task); // inform other clients
+	});
+
+	// listen for removing a task
+	socket.on('removeTask', (id) => {
+		tasks = tasks.filter((task) => task.id !== id); // remove from server array
+		socket.broadcast.emit('removeTask', id); // inform other clients
+	});
 });
